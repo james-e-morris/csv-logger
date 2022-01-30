b648155bfc729823ab70708985ac3f0006882245
nicoeft <nfrancisquelo2@gmail.com>
2022-01-30 02:35:03 +0100

    set self.name, propagated to get_logs + reverted black style

--- src/csv_logger/csv_logger.py
+++ src/csv_logger/csv_logger.py
@@ -10,9 +10,9 @@ from datetime import datetime
 
 class CsvFormatter(logging.Formatter):
     def format_msg(self, msg):
-        """Format the msg to csv string"""
+        '''Format the msg to csv string'''
         if isinstance(msg, list):
-            msg = ",".join(map(str, msg))
+            msg = ','.join(map(str, msg))
         return msg
 
     def format(self, record):
@@ -21,27 +21,35 @@ class CsvFormatter(logging.Formatter):
 
 
 class CsvRotatingFileHandler(RotatingFileHandler):
-    def __init__(self, fmt, datefmt, filename, max_size, max_files, header=None):
+    def __init__(self,
+                 fmt,
+                 datefmt,
+                 filename,
+                 max_size,
+                 max_files,
+                 header=None):
         # Format header string if needed
         self._header = header and CsvFormatter(fmt, datefmt).format_msg(header)
         # check if file exists
         self.file_pre_exists = path.exists(filename)
         # call parent file handler __init__
-        RotatingFileHandler.__init__(
-            self, filename, maxBytes=max_size, backupCount=max_files
-        )
+        RotatingFileHandler.__init__(self,
+                                     filename,
+                                     maxBytes=max_size,
+                                     backupCount=max_files)
         self.formatter = CsvFormatter(fmt, datefmt)
         # Write the header if delay is False and a file stream was created.
         if self.stream is not None and not self.file_pre_exists:
-            self.stream.write("%s\n" % self._header)
+            self.stream.write('%s\n' % self._header)
 
     def rotation_filename(self, default_name):
-        """Make log files counter before the .csv extension"""
-        s = default_name.rsplit(".", 2)
-        return "{}_{:0{}d}.csv".format(s[0], int(s[-1]), self.backupCount // 10 + 1)
+        '''Make log files counter before the .csv extension'''
+        s = default_name.rsplit('.', 2)
+        return '{}_{:0{}d}.csv'.format(s[0], int(s[-1]),
+                                       self.backupCount // 10 + 1)
 
     def doRollover(self):
-        """Prepend header string to each log file"""
+        ''' Prepend header string to each log file'''
         RotatingFileHandler.doRollover(self)
         if self._header is None:
             return
@@ -52,17 +60,15 @@ class CsvRotatingFileHandler(RotatingFileHandler):
 
 
 class CsvLogger(logging.Logger):
-    def __init__(
-        self,
-        filename: str,
-        name: str = None,
-        level=logging.INFO,
-        fmt: str = "%(asctime)s,%(message)s",
-        datefmt: str = "%Y/%m/%d %H:%M:%S",
-        max_size: int = 10485760,
-        max_files: int = 10,
-        header=None,
-    ):
+    def __init__(self,
+                 filename: str,
+                 name: str = None,
+                 level=logging.INFO,
+                 fmt: str = '%(asctime)s,%(message)s',
+                 datefmt: str = '%Y/%m/%d %H:%M:%S',
+                 max_size: int = 10485760,
+                 max_files: int = 10,
+                 header=None):
         """logger class to perform the csv logging
 
         Args:
@@ -76,36 +82,36 @@ class CsvLogger(logging.Logger):
             header (list[str], optional): header to prepend to csv files. Defaults to None
         """
         self.filename = filename
+        self.name = name or filename.rsplit(".", 1)[0]
         self.datefmt = datefmt
         if header and isinstance(header, str):
-            header = header.split(",")
+            header = header.split(',')
         self.header = header
 
         # make log file directories
         if path.dirname(filename):
             makedirs(path.dirname(filename), exist_ok=True)
         # init logger and add handler
-        logging.Logger.__init__(self, name or filename.rsplit(".", 1)[0], level)
-        handler = CsvRotatingFileHandler(
-            fmt, datefmt, filename, max_size, max_files, header
-        )
+        logging.Logger.__init__(self, self.name, level)
+        handler = CsvRotatingFileHandler(fmt, datefmt, filename, max_size,
+                                         max_files, header)
         self.addHandler(handler)
 
     def get_logs(self, evaluate=False):
-        """read all logs from file and return them as a list of lists
+        """ read all logs from file and return them as a list of lists
         Args:
             evaluate (bool, optional): attempt to evaluate string values using python eval()
         """
         all_logs = []
         # get all files matching filename pattern with or without enumerations
-        name = self.filename.rsplit(".", 1)[0]
-        extension = self.filename.rsplit(".", 1)[1]
-        logfiles = glob.glob(f"{name}*.{extension}")
+        name = self.name
+        extension = self.filename.rsplit('.', 1)[1]
+        logfiles = glob.glob(f'{name}*.{extension}')
         logfiles.sort(reverse=True)
         # read each log file and create list of csv logs
         for logfile in logfiles:
-            with open(logfile, newline="") as csvfile:
-                csvreader = csv.reader(csvfile, delimiter=",", quotechar="|")
+            with open(logfile, newline='') as csvfile:
+                csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
                 for row in csvreader:
                     if row != self.header:
                         if evaluate:
@@ -117,8 +123,7 @@ class CsvLogger(logging.Logger):
                                 except:
                                     try:
                                         new_row.append(
-                                            datetime.strptime(x, self.datefmt)
-                                        )
+                                            datetime.strptime(x, self.datefmt))
                                     except:
                                         new_row.append(x)
                             row = new_row
